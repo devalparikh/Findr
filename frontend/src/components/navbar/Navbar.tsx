125d4762c2b120eabb572f060f634ebf8a51c764
Alexandro Fuentelzas <alex271@live.com>
2020-12-22 16:16:19 -0500

    Updated navbar to eggshell

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <div className="Navbar">
-      <span style={{color:"#FF66229", paddingLeft:"10px"}}> Findr </span>
+      <span style={{color:"#F8F4E3", paddingLeft:"10px"}}> Findr </span>
     </div>
   );
 }
