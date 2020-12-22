6a55031da8aa6586008824318ff13e64f5382f2a
Alexandro Fuentelzas <alex271@live.com>
2020-12-22 16:12:17 -0500

    Added basic landing page

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <span style={{color:"#FF66229", paddingLeft:"10px"}}> Findr </span>
+    </div>
+  );
+}
+
+export default Navbar;
