77ed916f84eb36d4dd349d4d4c8d2ddd1a7f1970
Deval Parikh <devalpp@gmail.com>
2021-01-18 11:26:22 -0500

    bootstrap added

---
+++ frontend/src/components/cards/categoryCard/CategoryCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../card.css';
+
+function CategoryCard() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+  
+  export default CategoryCard;
--- frontend/src/containers/main/Main.tsx
+++ frontend/src/containers/main/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar'
+import CategoryCard  from '../../components/cards/categoryCard/CategoryCard'
 
 function Main() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <CategoryCard></CategoryCard>
     </div>
   );
 }
