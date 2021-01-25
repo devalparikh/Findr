8b94c1aa9d3519ad8fd0cdfed35992d09f1c28be
Deval Parikh <devalpp@gmail.com>
2021-01-25 17:13:49 -0500

    update prod dependencies and center cards on home page when on mobile

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -20,86 +20,86 @@ function Home() {
     'https://images.pexels.com/photos/680074/pexels-photo-680074.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
     'https://images.pexels.com/photos/2891884/pexels-photo-2891884.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
     'https://images.pexels.com/photos/1045534/pexels-photo-1045534.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-];
+  ];
 
   return (
-  <Container fluid className="full-container">
-    <Row className="home-row-container">
-      <Col xs={12} lg={6}>
-        <h1 className="row-title">Just for You</h1>
-        <Row>
-        <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          
-        </Row>
-      
-      </Col>
-       
-      <Col xs={12} lg={6}>
-        <h1 className="row-title">Recently Visited  </h1>
-        <Row>
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-        </Row>
-      
-      </Col>
-    </Row>
+    <Container fluid className="full-container">
+      <Row className="home-row-container">
+        <Col xs={12} lg={6}>
+          <h1 className="row-title">Just for You</h1>
+          <Row className="home-row">
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
 
-    <Row className="home-row-container">
-      <Col xs={12} lg={6}>
-        <h1 className="row-title">New Categories For You</h1>
-        <Row>
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Date" image={'https://images.pexels.com/photos/4255483/pexels-photo-4255483.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'} />
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Sports" image={basketballImage} />
-          
-        </Row>
-      
-      </Col>
-       
-      <Col xs={12} lg={6}>
-        <h1 className="row-title">Saved Categories </h1>
-        <Row>
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Date" image={'https://images.pexels.com/photos/4255483/pexels-photo-4255483.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'} /> 
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Sports" image={basketballImage} />
-          
-        </Row>
-      
-      </Col>
-    </Row>
+          </Row>
 
-    <Row className="home-row-container">
-      <Col xs={12} lg={6}>
-        <h1 className="row-title">Top Places</h1>
-        <Row>
-        <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
-          
-        </Row>
-      
-      </Col>
-       
-      <Col xs={12} lg={6}>
-        <h1 className="row-title"> Top Categories </h1>
-        <Row>
-        <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Sports" image={basketballImage} />
-          <CategoryCard title="Sports" image={basketballImage} />
-        </Row>
-      
-      </Col>
-    </Row>
-  </Container>
+        </Col>
+
+        <Col xs={12} lg={6}>
+          <h1 className="row-title">Recently Visited  </h1>
+          <Row className="home-row">
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+          </Row>
+
+        </Col>
+      </Row>
+
+      <Row className="home-row-container">
+        <Col xs={12} lg={6}>
+          <h1 className="row-title">New Categories For You</h1>
+          <Row className="home-row">
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Date" image={'https://images.pexels.com/photos/4255483/pexels-photo-4255483.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'} />
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Sports" image={basketballImage} />
+
+          </Row>
+
+        </Col>
+
+        <Col xs={12} lg={6}>
+          <h1 className="row-title">Saved Categories </h1>
+          <Row className="home-row">
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Date" image={'https://images.pexels.com/photos/4255483/pexels-photo-4255483.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'} />
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Sports" image={basketballImage} />
+
+          </Row>
+
+        </Col>
+      </Row>
+
+      <Row className="home-row-container">
+        <Col xs={12} lg={6}>
+          <h1 className="row-title">Top Places</h1>
+          <Row className="home-row">
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Test" location="Location" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+            <PostCard title="Basketball court with 4 hoops inside the community." location="Grand View-on-Hudson, New York" review={5.0} reviewCount={69} images={tempArray} />
+
+          </Row>
+
+        </Col>
+
+        <Col xs={12} lg={6}>
+          <h1 className="row-title"> Top Categories </h1>
+          <Row className="home-row">
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Sports" image={basketballImage} />
+            <CategoryCard title="Sports" image={basketballImage} />
+          </Row>
+
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
