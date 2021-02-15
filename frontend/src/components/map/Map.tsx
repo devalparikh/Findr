673e941381bc13d2d0713c5a2df37ca8bd36b25f
Deval Parikh <devalpp@gmail.com>
2021-02-14 21:31:49 -0500

    pin added

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import ReactMapGL from 'react-map-gl';
+import {useState, useCallback} from 'react';
+import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl';
+import Pin from './Pin';
 
 function MapBox() {
     const [state, setState] = React.useState({
         viewport: {
             latitude: 38.8298,
             longitude: -77.3074,
-            zoom: 8
+            zoom: 14
         }
     });
-    
+
     const ref = React.useRef();
-    
+
     /* eslint-disable */
     React.useEffect(() => {
         if (ref.current) {
@@ -29,6 +31,33 @@ function MapBox() {
     }, []);
     /* eslint-enable */
 
+
+
+    // Share new activity marker 
+    const [marker, setMarker] = useState({
+        latitude: 38.8298,
+        longitude: -77.3074
+      });
+      const [events, logEvents] = useState({});
+    
+      const onMarkerDragStart = useCallback(event => {
+        logEvents(_events => ({..._events, onDragStart: event.lngLat}));
+      }, []);
+    
+      const onMarkerDrag = useCallback(event => {
+        logEvents(_events => ({..._events, onDrag: event.lngLat}));
+      }, []);
+    
+      const onMarkerDragEnd = useCallback(event => {
+        logEvents(_events => ({..._events, onDragEnd: event.lngLat}));
+        setMarker({
+          longitude: event.lngLat[0],
+          latitude: event.lngLat[1]
+        });
+      }, []);
+
+
+      
     return (
         // @ts-ignore
         <div ref={ref} style={{ height: "100%", width: "100%" }}>
@@ -50,7 +79,20 @@ function MapBox() {
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
                 // mapStyle={'mapbox://styles/mapbox/dark-v9'}
                 mapStyle={'mapbox://styles/mapbox/streets-v11'}
-            />
+            >
+                <Marker
+                    longitude={marker.longitude}
+                    latitude={marker.latitude}
+                    offsetTop={-20}
+                    offsetLeft={-10}
+                    draggable
+                    onDragStart={onMarkerDragStart}
+                    onDrag={onMarkerDrag}
+                    onDragEnd={onMarkerDragEnd}
+                >
+                    <Pin size={25} />
+                </Marker>
+            </ReactMapGL>
         </div>
     );
 }
